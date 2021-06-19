8f9546f6b161f15d67852b5e71f449eab5125381
acuD1 <arsciand@student.42.fr>
2021-06-19 13:25:21 +0200

    draft

---
+++ includes/ft_traceroute.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_traceroute.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/26 15:25:59 by arsciand          #+#    #+#             */
+/*   Updated: 2021/06/19 13:19:52 by arsciand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TRACEOUTE_H
+# define FT_PING_H
+
+#include "shared_libft_ft_traceroute.h"
+
+# define FAILURE                2
+# define SUCCESS                0
+
+#endif
